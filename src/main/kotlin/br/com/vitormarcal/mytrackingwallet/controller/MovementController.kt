dff56e28c7c7fb18514b7377b082d1e99097737e
Vítor Marçal <git@vitormarcal.com.br>
2022-09-09 19:31:24 -0300

    movement fix

--- src/main/kotlin/br/com/vitormarcal/mytrackingwallet/controller/MovementController.kt
+++ src/main/kotlin/br/com/vitormarcal/mytrackingwallet/controller/MovementController.kt
@@ -1,11 +1,15 @@
 package br.com.vitormarcal.mytrackingwallet.controller
 
+import br.com.vitormarcal.mytrackingwallet.repository.domain.GroupedValuesByCategoryDTO
 import br.com.vitormarcal.mytrackingwallet.repository.domain.Movement
 import br.com.vitormarcal.mytrackingwallet.repository.domain.MovementRepository
 import org.springframework.format.annotation.DateTimeFormat
 import org.springframework.web.bind.annotation.*
+import java.math.BigDecimal
+import java.math.RoundingMode
 import java.time.LocalDate
 import java.util.*
+import javax.annotation.PostConstruct
 
 @RestController
 @RequestMapping("/movement")
@@ -21,13 +25,14 @@ class MovementController(private val movementRepository: MovementRepository) {
                 repeat(movement.numberOfInstallments) {
                     val installmentNumber = it + 1
                     val settleDate = list.lastOrNull()?.settleDate?.plusMonths(1) ?: movement.settleDate
+                    val monthlyValue = movement.value.divide(movement.numberOfInstallments.toBigDecimal().setScale(2), RoundingMode.HALF_UP).setScale(2)
                     list.add(Movement(name = "${movement.name} ($installmentNumber/${movement.numberOfInstallments})",
                             description = movement.description,
                             settleDate = settleDate,
                             movementedAt = movement.movementedAt,
                             categoryId = movement.categoryId,
                             credit = movement.credit,
-                            value = movement.value,
+                            value = monthlyValue,
                             numberOfInstallments = movement.numberOfInstallments,
                             installmentNumber = installmentNumber,
                             hash = hash))
@@ -51,6 +56,20 @@ class MovementController(private val movementRepository: MovementRepository) {
     @GetMapping("values")
     fun groupyValuesByCategory(
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) startDate: LocalDate,
-            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) endDate: LocalDate) =
-            movementRepository.groupSumValuesByCategoryInSettleDatePeriod(startDate = startDate, endDate = endDate)
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) endDate: LocalDate): TotalMovement =
+            movementRepository.groupSumValuesByCategoryInSettleDatePeriod(startDate = startDate, endDate = endDate).let {
+                val total = it.map { movement -> movement.getTotalValue() }
+                        .fold(BigDecimal.ZERO, BigDecimal::add)
+
+                return TotalMovement(
+                       total = total,
+                        totalByCategory = it
+                )
+            }
+
+    class TotalMovement(
+            val total: BigDecimal,
+            val totalByCategory: List<GroupedValuesByCategoryDTO>
+    )
+
 }
